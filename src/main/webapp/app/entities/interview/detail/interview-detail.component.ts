367f1921618b9d15a6072aa6014a6883291dd003
Ali <alifastith@gmail.com>
2022-03-15 22:34:27 -0400

    interview details page is displaying

--- src/main/webapp/app/entities/interview/detail/interview-detail.component.ts
+++ src/main/webapp/app/entities/interview/detail/interview-detail.component.ts
@@ -10,14 +10,17 @@ import { Observable, take } from 'rxjs';
   templateUrl: './interview-detail.component.html',
 })
 export class InterviewDetailComponent implements OnInit {
-  interviewDetails!: Observable<InterviewDetailsDTO>;
+  interviewDetails!: InterviewDetailsDTO;
   private interviewId!: number;
 
   constructor(protected interviewService: InterviewService, protected activatedRoute: ActivatedRoute) {}
 
   ngOnInit(): void {
     this.activatedRoute.paramMap.pipe(take(1)).subscribe(paramMap => (this.interviewId = Number(paramMap.get('id'))));
-    this.interviewService.getInterviewDetails(this.interviewId).pipe(take(1)).subscribe();
+    this.interviewService
+      .getInterviewDetails(this.interviewId)
+      .pipe(take(1))
+      .subscribe(interview => (this.interviewDetails = interview));
   }
 
   previousState(): void {
